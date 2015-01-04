6a25672fb2205e203bcdb69880e3a25684a495b4
bmwasaru <bmwasaru@gmail.com>
2015-01-04 03:03:27 +0300

    templates, includes, styling

---
+++ client/templates/feelings/feeling_item.js
@@ -0,0 +1,6 @@
+// TODO: Trows an  "Exception in template helper: TypeError: undefined is not a function"
+//Template.feelingItem.helpers({
+//    submittedDate: function() {
+//        return this.date.toLocaleTimeString();
+//    }
+//});
--- client/templates/feelings/feelings_list.js
+++ client/templates/feelings/feelings_list.js
@@ -1,5 +1,5 @@
 Template.feelingsList.helpers({
     feelings: function() {
-        return feelings.find();
+        return Feelings.find();
     }
 });
